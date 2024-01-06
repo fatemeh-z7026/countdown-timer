c2ae163b329918289bb4c2a199a63594a7def726
F-zeinali <fatemeh.zeinali7026@gmail.com>
2024-01-06 18:39:56 +0330

    check Nan

--- script.js
+++ script.js
@@ -7,27 +7,35 @@ let restBtn = $.getElementById("rest");
 let pauseBtn = $.getElementById("pause");
 let resumeBtn = $.getElementById("resume");
 let secondsRemaining;
+//a variable isRunning which tells tick() whether to keep going after each second of countdown
 let isRunning = false;
 let countDown;
 
+//a function tick() that can be called by startTimer() or resumeTimer()
 function tick() {
+  //every second, call the "update" function
+  //put into a variable to stop interval
   countDown = setInterval(update, 1000);
   isRunning = true;
+
   function update() {
     if (isRunning == false) {
       clearInterval(countDown);
     } else {
+      // turn the seconds into mm:ss
       let min = Math.floor(secondsRemaining / 60);
       let sec = secondsRemaining - min * 60;
-
+      //add a leading zero (as a string value) if seconds less than 10
       if (min < 10) {
         min = "0" + min;
       }
       if (sec < 10) {
         sec = "0" + sec;
       }
+      // now change the display
       timeDisplayMinute.innerHTML = min;
       timeDisplaySecond.innerHTML = sec;
+      // stop is down to zero
       if (secondsRemaining === 0) {
         clearInterval(countDown);
       }
@@ -37,10 +45,21 @@ function tick() {
 }
 
 function startTimer() {
+  // get countents of the "minutes" text box
   let minutes = document.getElementById("minutes").value;
+  // how many seconds
   secondsRemaining = minutes * 60;
-
+  // check if not a number
+  if (isNaN(minutes)) {
+    alert("Please Insert Number");
+    return; // stops function if true
+  }
+  if (minutes == "") {
+    stopTimer();
+  }
+  //start interval
   tick();
+  console.log("Start");
 }
 
 function stopTimer() {
@@ -63,5 +82,7 @@ restBtn.addEventListener("click", function () {
   clearInterval(countDown);
   timeDisplayMinute.innerHTML = "00";
   timeDisplaySecond.innerHTML = "00";
+  minutes.value = "";
+  minutes.placeholder = "Insert minute ";
   console.log("rested");
 });
